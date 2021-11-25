2ba69e07ef96f3d48b474d7916b0723cc42003f3
Damilare Agba <blankgodd33@gmail.com>
2021-11-25 11:07:17 +0100

    backend

---
+++ task/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Posts(models.Model):
+    user_id = models.IntegerField(default=0)
+    title = models.CharField(max_length=100)
+    content = models.TextField('content')
+    private = models.BooleanField(default=True)
+    pid = models.TextField('public id')
+    date_created = models.DateTimeField(auto_fill_now=True)
+
