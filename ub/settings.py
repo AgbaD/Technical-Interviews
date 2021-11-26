74ba2f84fdd370d078d0b08f7aba1658b8063e1d
Damilare Agba <blankgodd33@gmail.com>
2021-11-26 15:31:02 +0100

    working code

--- ub/settings.py
+++ ub/settings.py
@@ -124,9 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [(os.path.join(BASE_DIR, 'static'))]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-#  Add configuration for static files storage using whitenoise
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
