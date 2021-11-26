049a795cae10ae3509d0011b5d32c310cf251d1a
Damilare Agba <blankgodd33@gmail.com>
2021-11-26 15:06:49 +0100

    deploying

--- ub/settings.py
+++ ub/settings.py
@@ -22,7 +22,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x@mk@%z9zefl55qb^gl=j6kktnz!0($@+oxhdys^&n_v-f(=v*'
+SECRET_KEY = '@mk@%z9zefl55qb^gl=j6kktnz!0($@+oxhdys^&n_v-f(=v*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
